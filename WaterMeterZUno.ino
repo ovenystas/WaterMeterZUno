90e3e664b9d7c7b703c5e91df02c616be18c0c84
Ove Nystås <ove.nystas@gmail.com>
2019-12-03 01:18:54 +0100

    Improvements

--- WaterMeterZUno.ino
+++ WaterMeterZUno.ino
@@ -12,7 +12,7 @@
 
 #define VERSION "0.01"
 
-// channel number
+// channel numbers
 #define ZUNO_CHANNEL_WATER_METER 1
 #define ZUNO_CHANNEL_WATER_FLOW 2
 #define ZUNO_CHANNEL_WATER_TEMPERATURE 3
@@ -36,8 +36,6 @@
 
 #define DIV_ROUND_CLOSEST(n, d) ((((n) < 0) ^ ((d) < 0)) ? (((n) - (d)/2)/(d)) : (((n) + (d)/2)/(d)))
 
-byte node_id = 0;
-bool serial_connected = false;
 
 typedef struct meter_data
 {
@@ -45,10 +43,11 @@ typedef struct meter_data
   uint8_t  crc8;
 } MeterData_t;
 
+
 // Variables for water meter
 MeterData_t meter_data = {0};
 uint32_t last_update_ms = 0;
-bool data_updated = false;
+bool new_meter_data = false;
 bool triggered = false;
 uint32_t last_run_meter_ms = 0;
 
@@ -66,19 +65,17 @@ int temp_reported;
 bool temp_sensor_found = false;
 uint32_t last_run_temp_ms = 0;
 
-
+// Configure Z-Uno
 ZUNO_SETUP_DEBUG_MODE(DEBUG_ON);
 
+ZUNO_SETUP_SLEEPING_MODE(ZUNO_SLEEPING_MODE_ALWAYS_AWAKE);
 
 // Sensor multilevel type for water flow is not yet defined in Z-uno but defined in spec v9.
 // Unit: Liter per hour (l/h)
 #define ZUNO_SENSOR_MULTILEVEL_TYPE_WATER_FLOW 0x38
 #define SENSOR_MULTILEVEL_SCALE_LITER_PER_HOUR 0x00
 
-
-// next macro sets up the Z-Uno channels
-// in this example we set up 1 meter channel
-// you can read more on http://z-uno.z-wave.me/Reference/ZUNO_SENSOR_MULTILEVEL/
+// Set up the Z-Uno channels
 ZUNO_SETUP_CHANNELS(
   ZUNO_METER(
     ZUNO_METER_TYPE_WATER,
@@ -109,50 +106,60 @@ ZUNO_SETUP_CHANNELS(
 // For some cases use UART (Serial0/Serial1)
 // It's a most comfortable way for debugging
 // By default we use built-in USB CDC (Serial)
-#define MY_SERIAL Serial
+#define SERIAL Serial
 
 
 void setup()
 {
-  MY_SERIAL.begin(115200);
-
-  node_id = zunoNID();
+  // Setup serial port. Wait for input in 10 seconds.
+  //byte tmp;
+  SERIAL.begin(115200);
+  //SERIAL.setTimeout(10000);
+  //size_t bytesRead = SERIAL.readBytes(&tmp, 1);
+  delay(10000);
 
   meterSetup();
   tempSetup();
+
+//  if (bytesRead > 0)
+//  {
+    printVersion();
+    printNodeId();
+    printTempSensorAddress();
+//  }
+//  else
+//  {
+//    SERIAL.println("Timeout");
+//  }
 }
 
 
 void loop()
 {
-  if (!serial_connected && Serial.available() > 0)
-  {
-    (void)Serial.read(); // Drop it
-    serial_connected = true;
-    printVersion();
-    printNodeId();
-    printTempSensorAddress();
-  }
   meterCheck();
+  meterEepromUpdate();
+
   tempCheck();
 }
 
 
 void printVersion(void)
 {
-  MY_SERIAL.println(__FILE__ " v" VERSION ", " __DATE__ ", " __TIME__);
+  SERIAL.println(__FILE__ " v" VERSION ", " __DATE__ ", " __TIME__);
 }
 
 void printNodeId(void)
 {
+  byte node_id = zunoNID();
+
   if (node_id == 0)
   {
-    MY_SERIAL.println("WARN: Not included in any Z-Wave network");
+    SERIAL.println("WARN: Not included in any Z-Wave network");
   }
   else
   {
-    MY_SERIAL.print("Z-Wave NodeId: ");
-    MY_SERIAL.println(node_id);
+    SERIAL.print("Z-Wave NodeId: ");
+    SERIAL.println(node_id);
   }
 }
 
@@ -160,14 +167,22 @@ void printTempSensorAddress(void)
 {
   if (temp_sensor_found)
   {
-    MY_SERIAL.print("DS18B20 address: ");
+    SERIAL.print("DS18B20 address: ");
     printHex(addr1, sizeof(addr1));
-    MY_SERIAL.println();
+    SERIAL.println();
   }
   else
   {
-    MY_SERIAL.println("ERROR: DS18B20 sensor not found");
+    SERIAL.println("ERROR: DS18B20 sensor not found");
   }
+}
+
+void printMeterData(void)
+{
+  SERIAL.print("Meter data: ticks=");
+  SERIAL.print(meter_data.ticks);
+  SERIAL.print(", crc=");
+  printHex(&meter_data.crc8, 1);
 }
 
 // Prints 8-bit data in hex with leading zeroes
@@ -177,10 +192,10 @@ void printHex(uint8_t* data_p, uint8_t length)
   {
     if (data_p[i] < 0x10)
     {
-      MY_SERIAL.print('0');
+      SERIAL.print('0');
     }
-    MY_SERIAL.print(data_p[i], HEX);
-    MY_SERIAL.print(' ');
+    SERIAL.print(data_p[i], HEX);
+    SERIAL.print(' ');
   }
 }
 
@@ -193,11 +208,13 @@ void meterSetup(void)
   // Get last meter values from EEPROM
   EEPROM.get(EEPROM_ADDR, &meter_data, sizeof(meter_data));
 
+  printMeterData();
+
   // Check data  
   if (crc_calc((uint8_t*)&meter_data.ticks, sizeof(meter_data.ticks)) != meter_data.crc8)
   {
     // Invalid data - reset all
-    MY_SERIAL.println("Bad eeprom crc8 - init meter data");
+    SERIAL.println("Bad eeprom crc8 - init meter data");
     meter_data.ticks = 0;
     updateMeterData();
   }
@@ -212,7 +229,7 @@ void tempSetup(void)
 // TODO: Change to interrupt for meter sensor pin
 void meterCheck(void)
 {
-  if (millis() - last_run_meter_ms < METER_CHECK_INTERVAL_MS)
+  if ((millis() - last_run_meter_ms) < METER_CHECK_INTERVAL_MS)
   {
     return;
   }
@@ -223,55 +240,59 @@ void meterCheck(void)
   {
     if (!triggered)
     {
+      triggered = true;
+
       // Calculate water flow in l/h
       uint32_t new_pulse_ms = millis();
       if (last_pulse_ms > 0)
       {
         uint32_t dt_ms = new_pulse_ms - last_pulse_ms;
         water_flow_lph = (uint16_t)((TICK_VALUE * MS_PER_HOUR) / dt_ms);
-        MY_SERIAL.print("Water flow: ");
-        MY_SERIAL.print(water_flow_lph);
-        MY_SERIAL.println(" l/h");
-        if (millis() - last_water_flow_report_ms >= SENSOR_MULTILEVEL_REPORT_INTERVAL)
+        SERIAL.print("Water flow: ");
+        SERIAL.print(water_flow_lph);
+        SERIAL.println(" l/h");
+        if ((millis() - last_water_flow_report_ms) >= SENSOR_MULTILEVEL_REPORT_INTERVAL)
         {
           last_water_flow_report_ms = millis();
           zunoSendReport(ZUNO_CHANNEL_WATER_FLOW);
-          MY_SERIAL.println("Water flow report sent");
+          SERIAL.println("Water flow report sent");
         }
       }
       last_pulse_ms = new_pulse_ms;
 
       // Add tick to water meter
       meter_data.ticks++;
-      data_updated = true;
-      triggered = true;
-      MY_SERIAL.print("Water meter: ");
-      MY_SERIAL.print(meter_data.ticks * TICK_VALUE);
-      MY_SERIAL.println(" l");
+      new_meter_data = true;
+      SERIAL.print("Water meter: ");
+      SERIAL.print(meter_data.ticks * TICK_VALUE);
+      SERIAL.println(" l");
       zunoSendReport(ZUNO_CHANNEL_WATER_METER);
-      MY_SERIAL.println("Water meter report sent");
+      SERIAL.println("Water meter report sent");
     }
   }
   else
   {
     triggered = false;
   }
-    
+}
+
+void meterEepromUpdate(void)
+{
   // To save EEPROM from a lot of r/w operation 
   // write it once in EEPROM_UPDATE_INTERVAL_MS if data was updated
-  if (data_updated && (millis() - last_update_ms) > EEPROM_UPDATE_INTERVAL_MS)
+  if (new_meter_data && (millis() - last_update_ms) > EEPROM_UPDATE_INTERVAL_MS)
   {
     last_update_ms =  millis();
 
     updateMeterData();
-    data_updated = false;
-    MY_SERIAL.println("EEPROM updated");
+    new_meter_data = false;
+    SERIAL.println("EEPROM updated");
   }
 }
 
 void tempCheck(void)
 {
-  if (!temp_sensor_found || millis() - last_run_temp_ms < TEMP_CHECK_INTERVAL_MS)
+  if (!temp_sensor_found || (millis() - last_run_temp_ms) < TEMP_CHECK_INTERVAL_MS)
   {
     return;
   }
@@ -283,27 +304,28 @@ void tempCheck(void)
   {
     water_temperature = DIV_ROUND_CLOSEST(tempC100, 10);
 
-    MY_SERIAL.print("TempC100: ");
-    MY_SERIAL.print(tempC100);
-    MY_SERIAL.print(", Temp: ");
-    MY_SERIAL.println(water_temperature);
+    SERIAL.print("TempC100: ");
+    SERIAL.print(tempC100);
+    SERIAL.print(", Temp: ");
+    SERIAL.println(water_temperature);
 
     if (abs(water_temperature - temp_reported) >= TEMP_DIFF_REPORT)
     {
       zunoSendReport(ZUNO_CHANNEL_WATER_TEMPERATURE);
       temp_reported = water_temperature;
-      MY_SERIAL.println("Water temperature report sent");
+      SERIAL.println("Water temperature report sent");
     }
   }
   else
   {
-    MY_SERIAL.println("ERROR: Invalid temperature");
+    SERIAL.println("ERROR: Invalid temperature");
   }
 }
 
 void updateMeterData(void)
 { 
   meter_data.crc8 = crc_calc((uint8_t*)&meter_data.ticks, sizeof(meter_data.ticks));
+  printMeterData();
   EEPROM.put(EEPROM_ADDR, &meter_data, sizeof(meter_data));
 }
 
@@ -311,7 +333,7 @@ void resetWaterMeter(void)
 {
   meter_data.ticks = 0;
   updateMeterData();
-  MY_SERIAL.println("Meter was reset");
+  SERIAL.println("Meter was reset");
 }
 
 dword getWaterMeter(void)
